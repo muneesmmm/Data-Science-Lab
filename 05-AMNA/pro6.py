629b02682e3c706619f141252fe34223ab42d8b0
AmnaRabeeh <74949775+AmnaRabeeh@users.noreply.github.com>
2021-11-25 15:18:47 +0530

    Add files via upload

---
+++ 05-AMNA/pro6.py
@@ -0,0 +1,9 @@
+from datetime import date
+fdate = date(2020, 5, 21)
+sdate = date(2021, 5, 18)
+print("First Date",fdate)
+print("Second Date",sdate)
+if fdate<sdate:
+    print(fdate)
+else:
+    print(sdate)
