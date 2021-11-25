3b5c5da9e002871c75b7f92087a5fb0f39a998a0
SHAHARBAN123 <78188636+SHAHARBAN123@users.noreply.github.com>
2021-11-25 15:23:25 +0530

    Add files via upload

---
+++ 48-SHAHARBAN MP/48 SHAHARBAN MP/pgm8.py
@@ -0,0 +1,10 @@
+
+   
+with open("ab.txt") as f:
+    with open("cd.txt","w") as f1:
+        for line in f:
+            f1.write(line)
+fi=open("cd.txt")
+li=fi.readlines()
+for line in li:
+    print(line)
