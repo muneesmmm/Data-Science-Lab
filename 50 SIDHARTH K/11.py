40e090a71eb89ed67ef12b6e658247e4b9f74d3d
Sidharth K <60838265+Sidharthsidhu@users.noreply.github.com>
2021-11-25 15:09:37 +0530

    Add files via upload

---
+++ 50 SIDHARTH K/11.py
@@ -0,0 +1,9 @@
+file = open("13a.py","r")
+counter = 0
+content = file.read()
+colist = content.split("\n")
+for i in colist:
+    if i:
+        counter += 1
+print("This is the number of lines in the file")
+print (counter)
