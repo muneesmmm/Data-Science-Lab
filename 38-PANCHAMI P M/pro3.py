991cebd06f3e1f5ff8c9e73893c52a19b2c690eb
PANCHAMI1999 <75013073+PANCHAMI1999@users.noreply.github.com>
2021-11-25 15:40:54 +0530

    Delete pro3.py

--- 38-PANCHAMI P M/pro3.py
+++
@@ -1,7 +0,0 @@
-tup1=('back','space')
-tup2=('1','2')
-print("original key tuple is:"+str(tup1))
-print("original value tuple is:"+str(tup2))
-if len(tup1)==len(tup2):
-    res=dict(zip(tup1,tup2))
-print("dictionary:"+str(res))
