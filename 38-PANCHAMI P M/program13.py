6bee1c227091bde30f1049ed44e5002b9acec770
PANCHAMI1999 <75013073+PANCHAMI1999@users.noreply.github.com>
2021-12-01 14:57:23 +0530

    Add files via upload

--- 38-PANCHAMI P M/program13.py
+++ 38-PANCHAMI P M/program13.py
@@ -1,11 +1,18 @@
 import numpy
-x=numpy.array([[1,2],[2,2]])
-y=numpy.array([[2,1],[1,1]])
+x=numpy.array([[1,2,3],[3,4,1],[4,5,6]])
+y=numpy.array([[4,3,2],[2,1,0],[2,3,1]])
 print("Matrix addition is:")
 print(numpy.add(x,y))
 print("Matrix subtraction is:")
 print(numpy.subtract(x,y))
 print("Matrix maultiplication is:")
 print(numpy.multiply(x,y))
-print("Matrix transpose is:")
-print(x,T)
+print("transpose of matrix is:")
+print(numpy.transpose(x))
+print(numpy.transpose(y))
+print("Scalar multiplication is:")
+z=2*x
+w=2*y
+print(z)
+print(w)
+
