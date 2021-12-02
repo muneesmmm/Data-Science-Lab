c391e0bc894ed0c64cb6ef542d7e9a22b6dcc3f3
haibzzhbz <74949778+haibzzhbz@users.noreply.github.com>
2021-12-02 15:21:27 +0530

    Add files via upload

---
+++ 20-Hiba Nafeesath/11th.py
@@ -0,0 +1,9 @@
+file=open('11.txt','r')
+counter=0
+content=file.read()
+colist=content.split("\n")
+for i in colist:
+    if i:
+        counter+=1
+print("This is the number of line in the file")
+print(counter)
