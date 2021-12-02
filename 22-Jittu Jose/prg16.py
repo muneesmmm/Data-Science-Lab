40b6d9c97cc375dc3040ae99c4bd67a739529114
jittujose <74975969+jittujose@users.noreply.github.com>
2021-12-02 16:26:37 +0530

    Add files via upload

---
+++ 22-Jittu Jose/prg16.py
@@ -0,0 +1,12 @@
+import numpy
+import scipy.linalg
+A = matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(A)
+u,s,v=svd(A)
+print(u)
+print(s)
+print(v)
+Sigma=diag(s)
+print(Sigma)
+b=numpy.multiply(u,numpy.multiply(s,v))
+print(b)
