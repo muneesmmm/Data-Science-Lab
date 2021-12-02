74b8374e8efd3679279a1cd4a1feb491b35b9d88
sadhiq421 <94596310+sadhiq421@users.noreply.github.com>
2021-12-02 15:10:57 +0530

    Delete p8.py

--- p8.py
+++
@@ -1,8 +0,0 @@
-with open("f.txt") as a:
-    with open("f1.txt","w") as b:
-        for line in a:
-            b.write(line)
-fi=open("f1.txt")
-li=fi.readlines()
-for line in li:
-    print(line)
