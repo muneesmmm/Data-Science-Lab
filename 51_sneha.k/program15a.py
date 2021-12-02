55a8a370290582adcd89a41d2b2df7c28eee406f
snehak2220 <74949282+snehak2220@users.noreply.github.com>
2021-12-02 16:00:27 +0530

    Add files via upload

--- 51_sneha.k/program15a.py
+++ 51_sneha.k/program15a.py
@@ -1,5 +1,5 @@
 import numpy
 def translationMatrix(tx=0, ty=0,tz=0):
-    return numpy.matrix([[1,0,tx],[0,1,ty],[0,0, tz]])
+    return numpy.matrix([[1,0,0,0],[0,1,0,0],[0,0,1,0],[tx,ty,tz,1]])
 matrix=translationMatrix(1,1,1)
 print(matrix)
