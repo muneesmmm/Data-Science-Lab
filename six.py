fe6ceee7cc52ce744781927a911df7e357005e40
vasudevantv <vasudevantv@yahoo.co.in>
2021-12-02 14:23:06 +0530

    Delete six.py

--- six.py
+++
@@ -1,9 +0,0 @@
-from datetime import date
-fdate = date(2021, 4, 8)
-sdate = date(2022, 4, 8)
-print("First Date",fdate)
-print("Second Date",sdate)
-if fdate<sdate:
-    print(fdate)
-else:
-    print(sdate)
