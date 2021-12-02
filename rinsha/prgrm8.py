dd025fb014dba5f7b8597343850e34b935448d2e
rinsha-star <74965580+rinsha-star@users.noreply.github.com>
2021-12-02 15:35:07 +0530

    Add files via upload

---
+++ rinsha/prgrm8.py
@@ -0,0 +1,8 @@
+with open("file.txt") as f:
+    with open("ss.txt","w") as f1:
+        for line in f:
+            f1.write(line)
+fi=open("ss.txt")
+li=fi.readlines()
+for line in li:
+    print(line)
