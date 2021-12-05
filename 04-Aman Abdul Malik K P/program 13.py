14f1c509d285cb7b37a8b4befb1d29f03337204e
Aman Abdul Malik K P <74949108+amanabdulmalik@users.noreply.github.com>
2021-12-05 22:58:29 +0530

    Update program 13.py

--- 04-Aman Abdul Malik K P/program 13.py
+++ 04-Aman Abdul Malik K P/program 13.py
@@ -1,18 +1,23 @@
 import numpy
 matrix1=numpy.matrix([[1,2,3],[4,5,6],[7,8,9]])
 matrix2=numpy.matrix([[10,11,12],[13,14,15],[16,17,18]])
+# Matrix Addition
 matrix3=numpy.add(matrix1,matrix2)
 print("matrix after addition")
 print(matrix3)
+# Matrix Subtraction
 matrix4=numpy.subtract(matrix1,matrix2)
 print("matrix after subtraction")
 print(matrix4)
+# Matrix Multiplication
 matrix5=numpy.matmul(matrix1,matrix2)
 print("matrix after multiplication")
 print(matrix5)
+# Scalar Multiplication
 matrix6=2*matrix1
 print("matrix1 after scalar multiplication")
 print(matrix6)
+# Matrix Transpose
 matrix7=numpy.transpose(matrix1)
 print("matrix1 after transpose")
 print(matrix7)
