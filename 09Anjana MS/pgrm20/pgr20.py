b8f5b2b67c3e82d00ac657f24de569e531d9cb3a
Anjana M S <75066223+Anjana-ms@users.noreply.github.com>
2021-12-06 10:54:23 +0530

    Update pgr20.py

--- 09Anjana MS/pgrm20/pgr20.py
+++ 09Anjana MS/pgrm20/pgr20.py
@@ -9,8 +9,8 @@ grouped = df.groupby('species')
 # group represents the grouped data frame
 # draws the scatter plot for each group
 for key, group in grouped:
-    group.plot(ax=ax, kind='scatter', x='petal.length', y='petal.width', label=key, color=colors[key])
+    group.plot(ax=ax, kind='scatter', x='sepal.length', y='sepal.width', label=key, color=colors[key])
 ax.set_title("Scatter Plot")
-ax.set_xlabel('Petal Length')
-ax.set_ylabel('Petal Width')
+ax.set_xlabel('sepal Length')
+ax.set_ylabel('sepal Width')
 pyplot.show()
