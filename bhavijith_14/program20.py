f3afe8cfc559216817eaa2a590b0cc65ef762ac8
bhavi-web <75049480+bhavi-web@users.noreply.github.com>
2021-12-06 10:35:56 +0530

    Add files via upload

---
+++ bhavijith_14/program20.py
@@ -0,0 +1,19 @@
+
+from matplotlib import pyplot
+import pandas
+df = pandas.read_csv('iris.csv')
+fig, ax = pyplot.subplots(1,1)
+# Creates a dictionary of colour values of each species
+colors = {'Setosa':'red', 'Versicolor':'green',
+'Virginica':'blue'}
+# Groups the data based on species values
+grouped = df.groupby('species')
+# group represents the grouped data frame
+# draws the scatter plot for each group
+for key, group in grouped:
+    group.plot(ax=ax, kind='scatter', x='petal.length',
+    y='petal.width', label=key, color=colors[key])
+ax.set_title("Scatter Plot")
+ax.set_xlabel('Petal Length')
+ax.set_ylabel('Petal Width')
+pyplot.show()
