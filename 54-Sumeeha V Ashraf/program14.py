50f26962d39e2a96fc759907550b985dcd829fc7
Sumeeha-Ashraf <72332068+Sumeeha-Ashraf@users.noreply.github.com>
2021-12-06 10:39:11 +0530

    Add files via upload

---
+++ 54-Sumeeha V Ashraf/program14.py
@@ -0,0 +1,24 @@
+import numpy
+def translationMatrix(tx=0, ty=0):
+    return numpy.matrix([[1,0,tx],
+                         [0,1,ty],
+                         [0,0, 1]])
+matrix=translationMatrix(1,1)
+print("Translation:")
+print(matrix)
+def rotationMatrix(degree):
+    theta = numpy.radians(degree)
+    c,s=numpy.cos(theta),numpy.sin(theta)
+    return numpy.matrix([[c,-s,0],
+                         [s,c,0],
+                         [0,0,1]])
+matrix=rotationMatrix(30)
+print("\nRotation:")
+print(matrix)
+def scalingMatrix(sx=0, sy=0):
+    return numpy.matrix([[sx,0,0],
+                         [0,sy,0],
+                         [0, 0,1]])
+smatrix=scalingMatrix(2,2)
+print("\nScaling:")
+print(smatrix)
