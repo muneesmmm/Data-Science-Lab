2017545c5ce38a562bce2a248caab4cd88c7b8fb
AKSHAYKUMARMR <74973377+AKSHAYKUMARMR@users.noreply.github.com>
2021-12-10 10:57:01 +0530

    Add files via upload

---
+++ 03-AKSHAYKUMAR M R/10.py
@@ -0,0 +1,8 @@
+search_text="hi"
+replace_text="hello"
+with open(r'sample.txt','r') as file:
+    data=file.read()
+    data=data.replace(search_text,replace_text)
+with open(r'sample.txt','w') as file:
+    file.write(data)
+print("text replaced")
