58333c93a38a02257fa05152c8560e509e53f928
sanaproject <84702536+sanaproject@users.noreply.github.com>
2021-12-22 10:58:33 +0530

    Add files via upload

---
+++ 17fathimasana/prg12.py
@@ -0,0 +1,11 @@
+def countStrings(n):
+    a=[0]*n
+    b=[0]*n
+    a[0]=b[0]=1
+    for i in range(1,n):
+        a[i]=a[i-1]+b[i-1]
+        b[i]=a[i-1]
+    return(1<<n)-a[n-1]-b[n-1]
+print(countStrings(5))
+© 2021 GitHub, Inc.
+Terms
