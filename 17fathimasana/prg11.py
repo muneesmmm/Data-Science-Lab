2e6affb4f5de04bc95d51bad0c534fee66180355
sanaproject <84702536+sanaproject@users.noreply.github.com>
2021-12-22 10:59:49 +0530

    Delete 17fathimasana directory

--- 17fathimasana/prg11.py
+++
@@ -1,9 +0,0 @@
-file=open("ab.txt","r")
-counter=0
-content=file.read()
-colist=content.split("\n")
-for i in colist:
-    if i:
-        counter+=1
-        print("this is the no: of lines in the file")
-        print(counter)
