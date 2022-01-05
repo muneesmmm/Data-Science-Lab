1ba6bf12365677a50bf453bab15bf1bb30c2207f
M Munees <67566629+muneesmmm@users.noreply.github.com>
2022-01-05 16:17:39 +0530

    Delete 34-MUHAMMED MUNEES M directory

--- 34-MUHAMMED MUNEES M/program7.py
+++
@@ -1,5 +0,0 @@
-#this code for show the current date and 5 days before Current Date
-from datetime import date, timedelta
-dt = date.today() - timedelta(5)
-print('Current Date :',date.today())
-print('5 days before Current Date :',dt)
--- 34-MUHAMMED MUNEES M/program9/program9.py
+++
@@ -1,5 +0,0 @@
-#program to capitalise each word in a file
-file_gfg=open('1.txt','r')
-for line in file_gfg:
-    output=line.upper()
-print(output)
