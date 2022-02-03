0f9232f57724c098b3aac023d365ca13ff1ccbc0
KT VIMAL <74952173+VIMAL72@users.noreply.github.com>
2022-02-03 15:40:29 +0530

    Update prg14.py

--- 24-KT VIMAL/prg14.py
+++ 24-KT VIMAL/prg14.py
@@ -1,31 +1,23 @@
 import numpy
-def scalingMatrix(sx=0, sy=0):
-    return numpy.matrix([[sx,0,0],
-                         [0,sy,0],
-                         [0,0,1]])
-matrix=scalingMatrix(2,2)
+print("Transalation Matrix")
+def translationMatrix(tx=0, ty=0):
+    return numpy.matrix([[1,0,tx],
+    [0,1,ty],
+    [0,0, 1]])
+matrix=translationMatrix(1,1)
 print(matrix)
 
+print("Rotation")
 def rotationMatrix(degree):
-    theta = numpy.radians(degree)
-    axis=input("Enter the axis(x,y) to rotate about: ")
-    c,s=numpy.cos(theta),numpy.sin(theta)
-    if axis=='x':
-        return numpy.matrix([[1,0, 0],
-                         [0, c,-s],
-                         [0,s, c]])
-    elif axis=='y':
-        return numpy.matrix([[c, 0, s],
-                         [0, 1, 0],
-                         [-s,0, c]])
-    else:
-        print("Axis not correct")
+    theta=numpy.radians(degree)
+    c=numpy.cos(theta)
+    s=numpy.sin(theta)
+    return numpy.matrix([[c,-s,0],[s,c,0],[0,0,1]])
 matrix=rotationMatrix(30)
 print(matrix)
 
-def scalingMatrix(sx=0, sy=0):
-    return numpy.matrix([[sx,0,0],
-                         [0,sy,0],
-                         [0,0,1]])
-matrix=scalingMatrix(2,2)
+print("Scaling")
+def ScalingMatrix(sx=0,sy=0):    
+    return numpy.matrix([[sx,0,0],[0,sy,0],[0,0,1]])
+matrix=ScalingMatrix(2,2)
 print(matrix)
