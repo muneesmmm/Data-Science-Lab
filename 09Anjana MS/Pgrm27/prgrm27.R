e447dda215383d71a2adeddda12829d665664183
Anjana M S <75066223+Anjana-ms@users.noreply.github.com>
2022-02-04 11:52:10 +0530

    Add files via upload

---
+++ 09Anjana MS/Pgrm27/prgrm27.R
@@ -0,0 +1,15 @@
+library(C50)
+person<-read.csv("person.csv")
+person_train<-person[1:7,-5]
+person_test<-person[8:10,-5]
+person_train_labels<-person[1:7,5]
+person_test_labels<-person[8:10,5]
+person_model<-C5.0(person_train,as.factor(person_train_labels))
+person_model
+summary(person_model)
+person_pred<-pedict(person_model,person_test)
+person_pred<-predict(person_model,person_test)
+person_pred
+CrossTable(person_test_labels,person_pred,prop.chisq=FALSE)
+library(gmodels)
+CrossTable(person_test_labels,person_pred,prop.chisq=FALSE)
