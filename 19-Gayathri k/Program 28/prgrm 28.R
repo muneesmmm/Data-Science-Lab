8dc469343e3d409e97714c71a4a47543ff09b087
gayathri916 <74950081+gayathri916@users.noreply.github.com>
2022-02-04 11:54:51 +0530

    Add files via upload

---
+++ 19-Gayathri k/Program 28/prgrm 28.R
@@ -0,0 +1,12 @@
+library(C50)
+library(gmodels)
+cricket<-read.csv("cricket.csv")
+cricket_train<-cricket[1:10,-5]
+cricket_test<-cricket[11:14,-5]
+cricket_train_labels<-cricket[1:10,5]
+cricket_test_labels<-cricket[11:14,5]
+cricket_model<-C5.0(cricket_train,as.factor(cricket_train_labels))
+cricket_model
+cricket_pred<-predict(cricket_model,cricket_test)
+cricket_pred
+CrossTable(cricket_test_labels,cricket_pred,prop.chisq=FALSE)
