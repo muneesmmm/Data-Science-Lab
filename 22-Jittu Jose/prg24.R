ca355288b7dd7c16c2df52303b352d17b2548539
jittujose <74975969+jittujose@users.noreply.github.com>
2022-02-04 12:20:49 +0530

    Add files via upload

---
+++ 22-Jittu Jose/prg24.R
@@ -0,0 +1,12 @@
+print(getwd())
+library(e1071)
+symptoms = read.csv("symptoms.csv")
+symptoms_train = symptoms[,1:4]
+symptoms_train 
+symptoms_test = data.frame(Chills="Y",RunningNose="N", Headache="mild",Fever="Y")
+symptoms_test
+classifier_cl <- naiveBayes(symptoms_train,symptoms$HasFlu)
+classifier_cl
+symptoms_test_pred <- predict(classifier_cl, symptoms_test) 
+cat("Prediction of Flu:\n") 
+symptoms_test_pred
