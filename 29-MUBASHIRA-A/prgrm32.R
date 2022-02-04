88764bc9cc062a3dd3015c4c531e0fab9c173a97
MUBASHIRA A <74953415+MubashiraA@users.noreply.github.com>
2022-02-04 11:01:18 +0530

    Add files via upload

---
+++ 29-MUBASHIRA-A/prgrm32.R
@@ -0,0 +1,10 @@
+
+insurance<-read.csv("insurance.csv")
+insurance_train<-insurance[1:1000,]
+insurance_test<-insurance[1001:1338,]
+insurance_model<-lm(expenses ~ age + sex + bmi + children + smoker + region,data = insurance_train)
+insurance_model
+summary(insurance_model)
+insurance_pred<-predict(insurance_model,insurance_test)
+insurance_pred
+ 
