f219b40e16f975f64871039d56f5dbe0643d56c7
Anjana M S <75066223+Anjana-ms@users.noreply.github.com>
2022-02-07 21:01:37 +0530

    Update pgrm14.py

--- 09Anjana MS/09 Anjana ms/pgrm14.py
+++ 09Anjana MS/09 Anjana ms/pgrm14.py
@@ -1,7 +1,7 @@
 import numpy
 #Translation matrix
 def translationMatrix(tx=0, ty=0):
-  return numpy.matrix([[1,tx],[0,1]])
+    return numpy.matrix([[1,0,tx],[0,1,ty],[0,0, 1]])
 matrix=translationMatrix(1,1)
 print(matrix)
 #Rotation matrix
