797f8245dec03cc44c49eaa0722c3ad39a4ffd60
Abhijithmadakkayi <75051560+Abhijithmadakkayi@users.noreply.github.com>
2022-02-09 11:30:04 +0530

    Add files via upload

---
+++ 02-ABHIJITH M/27/person.R
@@ -0,0 +1,15 @@
+print(getwd())
+setwd("/Users/mes8/Desktop/Abhijith/27")
+library(C50)
+person<-read.csv("person.csv")
+person_train<-person[1:7,-5]
+person_test<-person[8:10,-5]
+person_train_labels<-person[1:7,5]
+person_test_labels<-person[8:10,5]
+person_model<-C5.0(person_train,as.factor(person_train_labels))
+person_model
+summary(person_model)
+person_pred<-predict(person_model,person_test)
+person_pred
+library(gmodels)
+CrossTable(person_test_labels,person_pred,prop.chisq=FALSE)
