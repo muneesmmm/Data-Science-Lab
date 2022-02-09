0ff26fcd2058f5c565c97ca29139d9245dfd9d99
Safeelanasrin <74948799+Safeelanasrin@users.noreply.github.com>
2022-02-09 14:01:51 +0530

    Add files via upload

---
+++ 42-Safeela Nasrin ck/prgm31/prgm31.R
@@ -0,0 +1,10 @@
+
+heart<-read.csv("heart.csv")
+heart_train<-heart[1:495,]
+heart_test<-heart[496:498,]
+heart_model<-lm(heart.disease~biking+smoking,data=heart_train)
+heart_model
+
+summary(heart_model)
+heart_pred<-predict(heart_model,heart_test)
+heart_pred
