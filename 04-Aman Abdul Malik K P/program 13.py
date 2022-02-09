af63d46882416aaed84163f3b4eee0380ae6d973
Aman Abdul Malik K P <74949108+amanabdulmalik@users.noreply.github.com>
2022-02-09 11:55:53 +0530

    Update program 13.py

--- 04-Aman Abdul Malik K P/program 13.py
+++ 04-Aman Abdul Malik K P/program 13.py
@@ -1,6 +1,6 @@
 import numpy
-matrix1=numpy.matrix([[1,2,3],[4,5,6],[7,8,9]])
-matrix2=numpy.matrix([[10,11,12],[13,14,15],[16,17,18]])
+matrix1=numpy.matrix1([[1,2,3],[4,5,6],[7,8,9]])
+matrix2=numpy.matrix2([[10,11,12],[13,14,15],[16,17,18]])
 # Matrix Addition
 matrix3=numpy.add(matrix1,matrix2)
 print("matrix after addition")
