17b54588655c09589e8f73377975382f358cc32c
Rinsha19 <74965580+Rinsha19@users.noreply.github.com>
2022-02-09 14:18:11 +0530

    Add files via upload

---
+++ 40-Rinsha ap/prgrm29/Bankloans.R
@@ -0,0 +1,13 @@
+library(C50)
+library(gmodels)
+credit<-read.csv("credit.csv")
+credit_train<-credit[1:900,-17]
+credit_test<-credit[901:1000,-17]
+credit_train_labels=credit[1:900,17]
+credit_test_labels=credit[901:1000,17]
+credit_model<-C5.0(credit_train,as.factor(credit_train_labels))
+credit_model
+summary(credit_model)
+credit_pred<-predict(credit_model,credit_test)
+credit_pred
+CrossTable(credit_test_labels,credit_pred,prop.chisq=FALSE)
