e81d5029318b15da8ab9b5807b7c98626b284067
Nabeelep <84263179+Nabeelep@users.noreply.github.com>
2022-02-16 16:08:52 +0530

    Delete 35_Nabeel Ep directory

--- 35_Nabeel Ep/python8.py
+++
@@ -1,8 +0,0 @@
-with open("1.txt") as f:
-    with open("2.txt","w") as f1:
-        for line in f:
-            f1.write(line)
-fi=open("1.txt")
-li=fi.readlines()
-for line in li:
-    print(line)
