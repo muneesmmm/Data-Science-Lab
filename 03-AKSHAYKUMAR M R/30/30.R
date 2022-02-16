647d1d2e36f2f1516fe5b47053f4efc616101f1d
safwana822 <75054371+safwana822@users.noreply.github.com>
2022-02-16 16:28:56 +0530

    Add files via upload

---
+++ 03-AKSHAYKUMAR M R/30/30.R
@@ -0,0 +1,9 @@
+print(getwd())
+setwd("/Users/mes22/Desktop/R/30")
+economy<-read.csv("economy.csv")
+economy_train<-economy[1:20,]
+economy_test<-economy[21:24,]
+economy_model<-lm(Stock_Index_Price~Interest_Rate+Unemployment_Rate,data=economy_train)
+economy_model
+economy_pred<-predict(economy_model,economy_test)
+economy_pred
---
+++ 03-AKSHAYKUMAR M R/31/31.R
@@ -0,0 +1,9 @@
+print(getwd())
+setwd("/Users/mes22/Desktop/R/31")
+heart<-read.csv("heart.csv")
+heart_train<-heart[1:495,]
+heart_test<-heart[496:498,]
+heart_model<-lm(heart.disease~biking+smoking,data=heart_train)
+heart_model
+heart_pred<-predict(heart_model,heart_test)
+heart_pred
