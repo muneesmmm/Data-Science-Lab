796e41d1175eaed9d70249ae43291c1c652eabac
devika354 <74951333+devika354@users.noreply.github.com>
2022-02-16 16:09:42 +0530

    Add files via upload

---
+++ 16_Devikaps/Dsml/30.R
@@ -0,0 +1,11 @@
+economy<-read.csv("economy.csv")
+economy_train<-economy[1:20,]
+economy_test<-economy[21:24,]
+economy_model<-lm(Stock_Index_Price~Interest_Rate+Unemployment_Rate,data=economy_train)
+economy_model
+economy_pred<-predict(economy-model,economy_test)
+economy_pred<-predict(economy_model,economy_test)
+economy_pred
+© 2022 GitHub, Inc.
+Terms
+Pr
