94a0f2c5b92026cbc585b10a1bceb056dda78dee
Shuhairvk <74948543+Shuhairvk@users.noreply.github.com>
2022-02-16 16:04:32 +0530

    Delete 33-shuhair.vk directory

--- 33-shuhair.vk/program1.py
+++
@@ -1,3 +0,0 @@
-lis = [19, 10, 45, 26, 6]
-print("Largest number of the list is:", max(lis))
-
--- 33-shuhair.vk/program10.py
+++
@@ -1,7 +0,0 @@
-fin = open("1.txt", "rt")
-str = fin.read()
-str = str.replace('pyton', 'python')
-fin.close()
-fin = open("1.txt", "wt")
-fin.write(str)
-fin.close()
